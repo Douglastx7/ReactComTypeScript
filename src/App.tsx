168f00e97dec69ae5f841cdea547a0d084d858c9
Douglastx7 <douglastx007@gmail.com>
2024-05-21 01:27:45 -0300

    Aula 0303

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AppAula2 from "./components/AppsAulas/AppAula2";
 import AppAula3 from "./components/AppsAulas/AppAula3";
 import AppAula4 from "./components/AppsAulas/AppAula4";
 import AppAula0301 from "./components/AppsAulas/AppAula0301";
+import AppAula0303 from "./components/AppsAulas/AppAula0303";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
           {/* <AppAula2/> */}
           {/* <AppAula3/> */}
           {/* <AppAula4/> */}
-          <AppAula0301/>
+          {/* <AppAula0301/> */}
+          <AppAula0303/>
       </div>
   )
 }
